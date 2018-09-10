164cd72566c391cb72ab1eb884fb4735191f1c0c
mjkim1411 <31556613+mjkim1411@users.noreply.github.com>
2018-09-10 09:16:10 +0900

    Update Cube.cs

--- Cube.cs
+++ Cube.cs
@@ -3,17 +3,14 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
-
-    void Start()
+   void Start()
     {
-        MeshFilter mf = GetComponent<MeshFilter>();
+    MeshFilter mf = GetComponent<MeshFilter>();
         Mesh m = new Mesh();
-
         m.vertices = GetCubeVertices(new Vector3(5f, 5f, 5f));
         m.triangles = GetQuadTriangles(0, 6);
         m.uv = GetUVArray(new Vector2(0.0f, 0.75f), new Vector3(0.125f, 0.125f, 0.125f));
         m.RecalculateNormals();
-
         mf.mesh = m;
     }
 
@@ -86,7 +83,6 @@ public class Cube : MonoBehaviour
     {
         List<Vector2> uvs = new List<Vector2>();
 
-
         Vector2[] top = new Vector2[]
         {
             new Vector2(origin.x + uvSize.z, origin.y + uvSize.y),
@@ -134,7 +130,6 @@ public class Cube : MonoBehaviour
             new Vector2(origin.x + (uvSize.z + uvSize.x) * 2, origin.y + uvSize.y),
             new Vector2(origin.x + (uvSize.z + uvSize.x) * 2, origin.y)
         };
-
         uvs.AddRange(top);
         uvs.AddRange(bottom);
         uvs.AddRange(front);

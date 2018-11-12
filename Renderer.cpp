152e3fd3110b793f96cdfb9caf5b83690ed48ab1
mjkim1411 <31556613+mjkim1411@users.noreply.github.com>
2018-11-12 19:11:30 +0900

    Update Renderer.cpp

--- Renderer.cpp
+++ Renderer.cpp
@@ -1,4 +1,3 @@
-
 #include "stdafx.h"
 #include "SoftRenderer.h"
 #include "GDIHelper.h"
@@ -33,19 +32,70 @@ void PutPixel(int x, int y)
 void UpdateFrame(void)
 {
 	// Buffer Clear
-	SetColor(0, 0, 0);
+	SetColor(32, 128, 255);
 	Clear();
 
+	float Cradius = 10.0f;
+	int Cnradius = (int)Cradius;
+	Vector3 center(0.0f, 0.0f);
+
+	// Circle
+	static float Cdegree = 0;
+	Cdegree += -1;
+	Cdegree = fmodf(Cdegree, 30.0f);
+
+	Matrix3 circleMoveMatrix;
+	circleMoveMatrix.SetTranslation(0.f, 0.f);
+
+	float CmaxPos = 1;
+	float Cpos = sinf(Deg2Rad(Cdegree)) * CmaxPos;
+	Matrix3 circleRotationMatrix;
+	circleRotationMatrix.SetTranslation(Cpos, Cpos);
+
+	Matrix3 circleMatrix = circleRotationMatrix * circleMoveMatrix;
+
+	// Circle Color
+	static Vector3 Ccolor;
+	Ccolor.X += 1;
+	Ccolor.Y += 0.5f;
+	Ccolor.Z += 1;
+
+	Ccolor.X = fmodf(Ccolor.X, 155.0f);
+	Ccolor.Y = fmodf(Ccolor.Y, 255.0f);
+	Ccolor.Z = fmodf(Ccolor.Z, 105.0f);
+
+	SetColor(Ccolor.X, Ccolor.Y, Ccolor.Z);
+
+	// Circle Draw
+	for (int i = -Cnradius; i <= Cnradius; i++)
+	{
+		for (int j = -Cnradius; j <= Cnradius; j++)
+		{
+			Vector3 vertex(i, j);
+			if (Vector3::Dist(center, vertex) <= Cradius)
+				PutPixel(Vector3(i, j) * circleMatrix);
+		}
+	}
+
+
 	// Draw
-	SetColor(156, 12, 156);
+	static Vector3 cor;
+	cor.X += 0.1f;
+	cor.Y += 0.2f;
+	cor.Z += 0.1f;
+
+	cor.X = fmodf(cor.X, 255.0f);
+	cor.Y = fmodf(cor.Y, 130.0f);
+	cor.Z = fmodf(cor.Z, 100.0f);
+
+	SetColor(cor.X, cor.Y, cor.Z);
 
 	// Draw a filled circle with radius 100
-	Vector3 center(0.0f, 0.0f);
-	float radius = 100.0f;
+	float radius = 30.0f;
 	int nradius = (int)radius;
 
 	static float degree = 0;
-	degree += 1;
+	degree += -1;
 	degree = fmodf(degree, 360.0f);
 
 	Matrix3 rotMat;
@@ -59,14 +109,14 @@ void UpdateFrame(void)
 	Matrix3 scaleMat;
 	scaleMat.SetScale(scale, scale, scale);
 
-	float maxPos = 150;
+	float maxPos = 110;
 	float pos = sinf(Deg2Rad(degree)) * maxPos;
 	Matrix3 translationMat;
-	translationMat.SetTranslation(pos, pos);
+	translationMat.SetTranslation(-pos, pos);
 
 	Matrix3 SR = scaleMat * rotMat;
 	Matrix3 TRS = translationMat * rotMat * scaleMat;
-	//Matrix3 SRT = translationMat * rotMat * scaleMat;
+
 	for (int i = -nradius; i <= nradius; i++)
 	{
 		for (int j = -nradius; j <= nradius; j++)
